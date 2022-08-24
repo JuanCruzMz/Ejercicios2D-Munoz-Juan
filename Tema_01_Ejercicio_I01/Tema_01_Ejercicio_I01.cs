33c0d870a3c8cd1a6fa52fa1a9070c00fb813266
Juan Cruz Muñoz <juancruzmchiappe1@gmail.com>
2022-08-23 21:31:45 -0300

    Add files via upload
    Primeros dos ejercicios de la guía

---
+++ Tema_01_Ejercicio_I01/Tema_01_Ejercicio_I01.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Tema_01_Ejercicio_I01
+{
+    internal class Tema_01_Ejercicio_I01
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio I1: Máximo, mínimo y promedio";
+
+            /*
+            Consigna:
+
+            Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+            */
+
+
+            int numero;
+            int max = 0;
+            int min = 0;
+            float promedio = 0;
+            int cantNumeros = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Ingresar número: ");
+
+                numero = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    min = numero;
+                    max = numero;
+                }
+
+                if (numero < min)
+                {
+                    min = numero;
+                }
+
+                if (numero > max)
+                {
+                    max = numero;
+                }
+
+                //En esta linea, "promedio" funciona como acumulador.
+                promedio = promedio + numero;
+
+                cantNumeros++;
+            }
+
+            promedio = promedio / cantNumeros;
+
+
+            Console.WriteLine($"El número máximo es: {max}, el número mínimo es: {min} y el promedio entre todos los números es: {promedio}");
+        }
+    }
+}
---
+++ Tema_01_Ejercicio_I02/Tema_01_Ejercicio_I02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tema_01_Ejercicio_I02
+{
+    internal class Tema_01_Ejercicio_I02
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio I2: Error al cubo";
+
+            /*
+            Consigna:
+
+            Ingresar un número y mostrar el cuadrado y el cubo del mismo. Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+
+            IMPORTANTE: Utilizar el método Pow de la clase Math para calcular las potencias.
+            */
+
+
+            int numero;
+            double cuadradoDelNumero;
+            double cuboDelNumero;
+
+            Console.Write("Ingrese un número: ");
+
+            numero = int.Parse(Console.ReadLine());
+
+
+            while (numero <= 0)
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            cuadradoDelNumero = Math.Pow(numero, 2);
+            cuboDelNumero = Math.Pow(numero, 3);
+
+
+            Console.WriteLine($"{numero} elevado al cuadrado es: {cuadradoDelNumero}\nY elevado al cubo es: {cuboDelNumero}");
+        }
+    }
+}
